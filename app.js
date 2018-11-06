033c903c212a08581aeeaeb9a0ed1dd8d59b1296
woobin <woobin0413@gmail.com>
2018-11-06 00:44:16 -0600

    ejs began

--- app.js
+++ app.js
@@ -2,15 +2,23 @@ var express = require('express');
 var cheerio = require('cheerio');
 var request = require('request');
 var app = express();
+app.set('view engine', 'ejs');
+app.set('views','./views/');
 
-request('https://datalab.naver.com', function (err, response, html) {
+app.get('/', function(req,res){
+  res.render('index');
+})
+request('https://www.naver.com/', function (err, response, html) {
   if(err) {
     res.status(500).send('Internal Server Error');
     console.log(err);
   } else {
       var $ = cheerio.load(html);
-      var footer_info = $('.footer_info');
+      var hot_chart = $('.ah_roll_area');
 
-      console.log(footer_info.text());
+      // console.log(footer_info.text());
+      console.log(hot_chart.text());
   }
 });
+app.listen(3000);
+console.log('3000 is the magic port');
